96e289c4297e4488c2f2d16dbf94eb0a2a838e74
budhirajamadhav <madhavbudhiraja131@gmail.com>
2021-02-06 03:25:52 +0530

    Work  - Feb 5, 2021

---
+++ JavaBasics/src/strings/CharAt.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.util.Scanner;
+
+public class CharAt {
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter name");
+		String name = sc.nextLine();
+		System.out.println(name.charAt(0));
+		
+	}
+
+}
---
+++ JavaBasics/src/strings/StringLength.java
@@ -0,0 +1,14 @@
+package strings;
+
+import java.util.Scanner;
+
+public class StringLength {
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("Enter a string");
+		String str = sc.nextLine();
+		System.out.println(str.length());
+	}
+	
+
+}
